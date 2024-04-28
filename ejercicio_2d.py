1d13c6f503fe99e322bb79138b681f9f1331281b
Sofia Arce <sofiaelianaarce@gmail.com>
2024-04-28 14:16:59 -0300

    add: ejercicio 2b parte crop nombres

---
+++ ejercicio_2d.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from ej2_b import obtener_renglon_de_datos, obtener_datos_de_campos
+
+# Funcion que devuelve los crop de los campos name
+def obtener_campo_nombre(examen):
+    renglon = obtener_renglon_de_datos(examen)
+    # Como sé que el ultimo campo en el nombre, me quedo con ese
+    name = obtener_datos_de_campos(renglon)[3]
+    #plt.figure(), plt.imshow(renglon, cmap='gray'),  plt.show(block=True)
+    return name
+
