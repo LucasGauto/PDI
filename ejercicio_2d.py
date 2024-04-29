543182cc27cafc072f9883b03777a3d748218cdf
Sofia Arce <sofiaelianaarce@gmail.com>
2024-04-29 13:46:36 -0300

    test

--- ejercicio_2d.py
+++ ejercicio_2d.py
@@ -6,11 +6,15 @@ from ej2_b import obtener_renglon_de_datos, obtener_datos_de_campos
 # Funcion que devuelve los crop de los campos name
 def obtener_campo_nombre(examen):
     renglon = obtener_renglon_de_datos(examen)
+    #plt.figure(), plt.imshow(renglon, cmap='gray'),  plt.show(block=True)
     # Como sé que el ultimo campo en el nombre, me quedo con ese
     campos = obtener_datos_de_campos(renglon)
     name = campos[3]
-    #plt.figure(), plt.imshow(renglon, cmap='gray'),  plt.show(block=True)
+    #plt.figure(), plt.imshow(name, cmap='gray'),  plt.show(block=True)
     return name
 
+
+
+obtener_campo_nombre('multiple_choice_1.png')
 
 
